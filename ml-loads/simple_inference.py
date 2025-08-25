447270ad975bc88879603d9584372dfc0196a0bf
Nikolai Rozanov <nikolai.rozanov@gmail.com>
2025-08-25 13:17:33 +0000

    working changes

--- ml-loads/simple_inference.py
+++ ml-loads/simple_inference.py
@@ -4,34 +4,52 @@ import time
 import cProfile
 import pstats
 
-def run_inference(num_gpus=4):
+import argparse
+
+def run_inference(num_gpus=1):
     sampling_params = SamplingParams(
         temperature=0.7,
         max_tokens=512,
         stop=["</s>"]
     )
+    print("\n\n\n===============\nSetting up LLM Engine:\n\n\n")
 
     llm = LLM(
-        model="Qwen/Qwen3-Coder-30B-A3B-Instruct",
+        # model="Qwen/Qwen3-Coder-30B-A3B-Instruct",
+        model="Qwen/Qwen2.5-7B-Instruct",
         tensor_parallel_size=num_gpus,
         trust_remote_code=True
     )
+    print("\n\n\n----\nSetting up LLM FINISHED\n\n\n")
 
     prompt = "Write a Python function to calculate the fibonacci sequence."
     
+    print(f"\n\n\n===============\nRunning Prompt: ```prompt\n{prompt}\n```\n\n\n")
+
     start_time = time.time()
     outputs = llm.generate([prompt], sampling_params)
     end_time = time.time()
+    print("\n\n\n----\nGeneration finished.\n\n\n")
 
     print(f"Generated text: {outputs[0].outputs[0].text}")
     print(f"Time taken: {end_time - start_time:.2f} seconds")
 
+def build_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num_gpus", type=int, default=1)
+    args = parser.parse_args()
+    return args
+
 if __name__ == "__main__":
-    profiler = cProfile.Profile()
-    profiler.enable()
+    print("Running script with args...")
+    args = build_args()
+    print(args)
+
+    # profiler = cProfile.Profile()
+    # profiler.enable()
+    print("--")
+    run_inference(num_gpus=args.num_gpus)
     
-    run_inference()
-    
-    profiler.disable()
-    stats = pstats.Stats(profiler).sort_stats('cumulative')
-    stats.print_stats()
+    # profiler.disable()
+    # stats = pstats.Stats(profiler).sort_stats('cumulative')
+    # stats.print_stats()
--- ml-loads/simple_inference_slurm.sh
+++ ml-loads/simple_inference_slurm.sh
@@ -2,19 +2,20 @@
 #SBATCH -J vllm_experiment
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1 #2
-#SBATCH --gpus-per-node=4 #8
+#SBATCH --gpus-per-node=8 #8
 #SBATCH -t 01:00:00
 #SBATCH --mem=128G #128G is not working #requesting more than 128 leads to an error.
 
 #SBATCH -p faculty
 #SBATCH --qos=gtqos    #the other option is stqos
 
-#SBATCH -oe ~/logs/vllm_experiment/%x-%j-%D.log
+#SBATCH -o ./logs/vllm_experiment/run_%j_%N_%s_%t.log
+#SBATCH -e ./logs/vllm_experiment/run_%j_%N_%s_%t.log
+
 
 # TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S") # Format: YYYY-MM-DD_HH-MM-SS
 
-
-mkdir -p ~/logs/vllm_experiment/
+mkdir -p ./logs/vllm_experiment/
 
 module load apptainer
 
@@ -26,7 +27,8 @@ mkdir -p logs
 # srun --ntasks=2 --gpus=4 \
 #   apptainer exec --rocm rocm-dev-5.7.sif \
 #   python train_ddp.py --data /path/to/dataset/ --out /vast/run_ddp
+NUM_GPUS=1
 
-srun --ntasks=1 --gpus=4 \
+srun --ntasks=1 --gpus=${NUM_GPUS} \
   apptainer exec --rocm ./apptainer_images/vllm-rocm.sif \
-  python ./ml-loads/simple_inference.py --out /vast/users/nikolai.rozanov/z_code/hpc-bench/logs
+  python ./ml-loads/simple_inference.py --num_gpus ${NUM_GPUS}
